6ba3b5e293a400df45052cf874693de2680cc2d0
Benjamin Cheng <bencheng200@gmail.com>
2021-05-29 21:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Your code here
-const mapToNegativize(sourceArray) = 
+const mapToNegativize = function(sourceArray) {
+  let newArr = []
+  for (i = 0; i < sourceArray.length; i++) {
+    newArr[i] = -sourceArray[i]
+  }
+  return newArr
+}
